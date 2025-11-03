33ff83650c7c377ebc311781cefdf28451455ce3
MuhammadRazalkv <rasalkv21@gmail.com>
2025-11-03 12:06:29 +0530

    ref issue resloved

--- src/routes/article.route.ts
+++ src/routes/article.route.ts
@@ -1,14 +1,15 @@
 import { Router } from 'express';
 import { authMiddleware } from '../middlewares/auth.middleware';
-const articleRepo = new ArticleRepo();
-const userRepo = new UserRepo();
-const articleService = new ArticleService(articleRepo, userRepo);
-const articleController = new ArticleController(articleService);
 import uploadMiddleware from '../utils/multer';
 import { ArticleRepo } from '../repository/article.repo';
 import ArticleService from '../service/article.service';
 import { UserRepo } from '../repository/user.repo';
 import ArticleController from '../controller/article.controller';
+
+const articleRepo = new ArticleRepo();
+const userRepo = new UserRepo();
+const articleService = new ArticleService(articleRepo, userRepo);
+const articleController = new ArticleController(articleService);
 const upload = uploadMiddleware('article-Images');
 const articleRoute = Router();
 articleRoute
